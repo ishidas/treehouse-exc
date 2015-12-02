94b6310b56f593392330d5832854ed8557d8e9c3
Sawako Ishida <sawako.yoriko@gmail.com>
2015-12-01 23:33:38 -0800

    added img and completed game

--- app2.js
+++ app2.js
@@ -1,5 +1,5 @@
 var qAnda = [
-  ['Question1: What is my favorite color?', 'My favorite color is Green.'],
+  ['Question1: What is my favorite color?', 'My favorite color is Green.',],
   ['Question2: Is my country of origin Japan?', 'Yes, it is.'],
   ['Question3: What is my favorite Japanese food?', 'Correct Answer is Ramen.']
 ];
@@ -12,14 +12,23 @@ function counter() {
   score += 1;
   }
 }
+
 //writing in Browser
 function writeInBrowser() {
   var oListElement = document.getElementById('questions');
   var listElement = document.createElement('li');
+  listElement.id = 'q'+ i;
   oListElement.appendChild(listElement);
-  listElement.id = 'q1';
   listElement.textContent = qAnda[i][0] + ' -' + qAnda[i][1]
   document.body.appendChild(oListElement);
+}
+
+//photo shows up when answer is correct
+function photoShow() {
+  var idOfListEl = document.getElementById('q'+i);
+  var imgEl = document.createElement('img');
+  imgEl.src = 'img/correct.jpeg'
+  idOfListEl.appendChild(imgEl);
 }
 
 for( var i = 0; i < qAnda.length; i += 1){
@@ -29,16 +38,19 @@ for( var i = 0; i < qAnda.length; i += 1){
     userAnswerCorrectly = true;
     writeInBrowser();
     counter();
+    photoShow();
   } else if(userAns.toUpperCase() === 'YES'){
     alert('You are correct!');
     userAnswerCorrectly = true;
     writeInBrowser();
     counter();
+    photoShow();
   } else if(userAns.toUpperCase() === 'RAMEN'){
     alert('You are correct!');
     userAnswerCorrectly = true;
     writeInBrowser();
     counter();
+    photoShow();
   } else {
     alert('Incorrect.');
     userAnswerCorrectly = false;
